b3913bcfa6f44412166a76048e7dc8dd1641b391
p23aelliff <p23aelliff@fairfieldprep.org>
2022-10-11 10:54:15 -0400

    lab

---
+++ lab_4-1.py
@@ -0,0 +1,11 @@
+#author ate (10/11/22)
+
+#Getting first and last name from input of user.
+fist_name = input("Aidan")
+last_name = input("Elliff")
+
+#Printing full name of user, did get some result.
+print(fist_name + last_name)
+
+#I am getting the inputs frgamented between two diferent runs.
+#It can be fix by potentially making the print a string or similar command.
