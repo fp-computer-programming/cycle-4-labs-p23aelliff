72b1bbd95a187687ed05c308db6c8c36a056d0b3
p23aelliff <p23aelliff@fairfieldprep.org>
2022-10-13 11:34:49 -0400

    Labs 2 and 3

---
+++ lab_4-2.py
@@ -0,0 +1,15 @@
+#author ate (10/11/22)
+
+school = "Fairfield Prep"
+
+#partial word slicing
+first_half = school[0:9]
+second_half = school[10:14]
+
+#full word slicing
+full_word = school[0:]
+
+#printing word
+print(first_half)
+print(second_half)
+print(full_word)
---
+++ lab_4-3.py
@@ -0,0 +1,12 @@
+#author ate (10/11/22)
+
+game = "Metal Gear Rising: Revegeance"
+
+#scaling game to see if equal, bigger, or smaller than Mario Bros game
+#then printing results
+if game < "Mario Bros":
+    print(game + " comes before Mario Bros")
+elif game > "Mario Bros":
+    print(game + " comes after Mario Bros")
+else:
+    print("Mario Bros for scale")
