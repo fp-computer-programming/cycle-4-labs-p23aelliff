b60c98c2caf9a3bf708281a56317e43edd9cc3b0
p23aelliff <p23aelliff@fairfieldprep.org>
2022-10-19 12:36:08 -0400

    lab 6-7

---
+++ lab_4-6.py
@@ -0,0 +1,18 @@
+#author ate (10/18/22)
+
+#names in each variables
+animal = "Crocodile"
+dish = "Cooc"
+
+print(dish.lower)
+print(animal.lower)
+
+#if, elif, else and print statements 
+if dish[0] == animal[0]:
+    if dish[3] == animal[0]:
+        print("Crocodile can bring his dessert to the feast")
+elif dish[0] != animal[0]:
+    if dish[3] != animal[0]:
+        print("Crocodile can not bring his dessert to the feast")
+else:
+    print("Crocodile should make a new dessert")
---
+++ lab_6-7.py
@@ -0,0 +1,11 @@
+#author ate (10/19/22)
+
+#variable containing string and sliced strings
+a = "1 2 3 4 5"
+b = a[0]
+c = a[8]
+
+#print statements for each variable
+print(a)
+print(b)
+print(c)
